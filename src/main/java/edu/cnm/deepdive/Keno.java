f5a8f338d7dba2fff28eb2ba64713d6552e57d30
zcr003 <zramsey1@cnm.edu>
2021-10-04 16:51:43 -0600

    Added pool_size and draw_size.

--- src/main/java/edu/cnm/deepdive/Keno.java
+++ src/main/java/edu/cnm/deepdive/Keno.java
@@ -6,14 +6,17 @@ import java.util.Random;
 
 public class Keno {
 
+  public static final int POOL_SIZE = 80;
+  public static final int DRAW_SIZE = 20;
+
   public static void main(String[] args) {
     Random random = new SecureRandom();
     Shuffler shuffler = new Shuffler(random);
-    int[] kenoPool = new int[80];
+    int[] kenoPool = new int[POOL_SIZE];
     for (int i = 0; i < kenoPool.length; i++) {
       kenoPool[i] = i + 1;
     }
-    int[] draw = shuffler.shuffle(kenoPool, 20);
+    int[] draw = shuffler.shuffle(kenoPool, DRAW_SIZE);
     Arrays.sort(draw);
     System.out.println(Arrays.toString(draw));
   }
