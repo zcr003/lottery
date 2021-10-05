f3a1184c45e414b4f3665d3cf5a965f629b60e35
zcr003 <zramsey1@cnm.edu>
2021-10-05 17:07:06 -0600

    Added helper method and overloads (in-progress).

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -13,8 +13,32 @@ public class Shuffler {
   }
 
   //two methods in the same class
+
   public void shuffle(int[] data) {
-    for (int sourceIndex = data.length - 1; sourceIndex > 0; sourceIndex--) {
+    shufflePart(data, data.length - 1);
+  }
+
+  public int[] shuffle(int[] data, int drawSize) {
+    shufflePart(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public void shuffle(double[] data) {
+    shufflePart(data, data.length - 1);
+  }
+
+  public double[] shuffle(double[] data, int drawSize) {
+    shufflePart(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  //This method was created by taking a for loop from above
+  //...Going to Refactor Extract/Introduce and choosing Method
+
+  // TODO Create overloads of both forms of shuffle (full and partial) for all the array types below.
+
+  private void shufflePart(int[] data, int drawSize) {
+    for (int sourceIndex = data.length - 1; sourceIndex >= data.length - drawSize; sourceIndex--) {
       int destIndex = rng.nextInt(sourceIndex + 1);
       if (sourceIndex != destIndex) {
         int temp = data[sourceIndex];
@@ -24,16 +48,17 @@ public class Shuffler {
     }
   }
 
-  public int[] shuffle(int[] data, int drawSize) {
+  private void shufflePart(double[] data, int drawSize) {
     for (int sourceIndex = data.length - 1; sourceIndex >= data.length - drawSize; sourceIndex--) {
       int destIndex = rng.nextInt(sourceIndex + 1);
       if (sourceIndex != destIndex) {
-        int temp = data[sourceIndex];
+        double temp = data[sourceIndex];
         data[sourceIndex] = data[destIndex];
         data[destIndex] = temp;
       }
     }
-    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
   }
+
+  // TODO Create overloads of shufflePart for boolean[], char[], short[], long[], float[], Object[]
 
 }
